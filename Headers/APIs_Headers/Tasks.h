1ffe8e6b65d0977c13ddff83f6daf3b7de887cb7
Mahmoud-Matar <70941488+Mahmoud-Matar@users.noreply.github.com>
2023-03-26 11:39:31 +0200

    Add files via upload

---
+++ Headers/APIs_Headers/Tasks.h
@@ -0,0 +1,36 @@
+#ifndef Headers_APIs_Headers_Tasks_H
+#define Headers_APIs_Headers_Tasks_H
+
+#define INVALID_TASK 0
+
+#define RUNNING 1
+#define READY 2
+#define WAITING 3
+#define SUSPENDED 4
+
+typdef uint8_t TaskType;
+typdef uint8_t TaskRefType;
+typdef uint8_t TaskStateType;
+typdef uint8_t TaskStateRefType;
+
+struct Task
+{
+	TaskType ID;
+	TaskStateType State;
+	uint8_t Priority;
+	uint8_t F_Preem; // flag of preemetivety 0 or 1
+	uint8_t Activation_Record;
+};
+
+
+StatusType ActivateTask(TaskType TaskID); // 1
+StatusType TerminateTask(void); //2 
+StatusType ChainTask(TaskType TaskID);
+StatusType Schedule(void);
+StatusType GetTaskID(TaskRefType TaskID); // 1
+StatusType GetTaskState(TaskType TaskID, TaskStateRefType State); //2
+void DeclareTask(int x);
+
+
+
+#endif
