398f889a8d2befc966e68e7bd2f0b1697b11dfce
Mahmoud-Matar <70941488+Mahmoud-Matar@users.noreply.github.com>
2023-03-26 11:41:21 +0200

    Add files via upload

---
+++ Configurations/TaskConfig.h
@@ -0,0 +1,18 @@
+#ifndef Configurations_TaskConfig_H
+#define Configurations_TaskConfig_H
+
+#define MAX_TASKS 16
+
+#define TASK_MAX_PRIO_NUM 5
+
+#define TASK_MIN_PRIO_NUM 0
+
+#define TASK_PER_PRIO 3
+
+#define PCB_TABLE_SIZE MAX_TASKS
+
+#define RESOURCES_PER_TASK 2
+
+
+
+#endif
