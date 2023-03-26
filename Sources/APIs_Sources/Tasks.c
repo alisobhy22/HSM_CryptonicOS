53805ec1cdad7c59ab5afc4c4bc8ec140a73b60c
Mahmoud-Matar <70941488+Mahmoud-Matar@users.noreply.github.com>
2023-03-26 12:01:35 +0200

    Add files via upload

---
+++ Sources/APIs_Sources/Tasks.c
@@ -0,0 +1,38 @@
+#include "..\..\Headers\APIs_Headers\Headers.h"
+
+
+
+StatusType ActivateTask(TaskType TaskID)
+{
+
+}
+
+StatusType TerminateTask(void)
+{
+
+}
+
+StatusType ChainTask(TaskType TaskID)
+{
+
+}
+
+StatusType Schedule(void)
+{
+
+}
+
+StatusType GetTaskID(TaskRefType TaskID)
+{
+
+}
+
+StatusType GetTaskState(TaskType TaskID, TaskStateRefType State)
+{
+
+}
+
+void DeclareTask(int x)
+{
+
+}
