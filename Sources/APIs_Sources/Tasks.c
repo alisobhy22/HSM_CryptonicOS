6483004a1d8a984a483af34cea565ce47bf2f785
Mahmoud-Matar <70941488+Mahmoud-Matar@users.noreply.github.com>
2023-03-29 16:00:16 +0200

    Update Tasks.c

--- Sources/APIs_Sources/Tasks.c
+++ Sources/APIs_Sources/Tasks.c
@@ -9,7 +9,35 @@ StatusType ActivateTask(TaskType TaskID)
 
 StatusType TerminateTask(void)
 {
+	// to be continued later, i.e to free reasourses
+	StatusType StatusMsg = E_OK;
+	if (RunningTaskID == INVALID_TASK) //  implement RunningTaskID later
+	{
+		StatusMsg = E_OS_ID;
+	}
+	else
+	{
+		if (OsTasksPCB[RunningTaskID - 1].Activation_Record > 0)
+		{
+			//Context_Switch();
+			OsTasksPCB[RunningTaskID - 1].State = READY;
+			// error msg?
+		}
+		else
+		if (OsTasksPCB[RunningTaskID - 1].Reasourses_Occupied != 0)
+		{
+			StatusMsg = E_OS_RESOURCE;
+		}
+		else
+		{
+			//Context_Switch();
+			OsTasksPCB[RunningTaskID - 1].State = SUSPENDED;
+			RunningTaskID = INVALID_TASK;
+		}
+		// return calllevel error msg when called from ISR...
 
+	}
+	return StatusMsg;
 }
 
 StatusType ChainTask(TaskType TaskID)
@@ -24,12 +52,33 @@ StatusType Schedule(void)
 
 StatusType GetTaskID(TaskRefType TaskID)
 {
-
+	StatusType StatusMsg = E_OK;
+	if (RunningTaskID == INVALID_TASK) //  implement RunningTaskID later
+	{
+		*TaskID = INVALID_TASK;
+		StatusMsg = E_OS_ID;
+	}
+	else
+	{
+		*TaskID = OsTasksPCB[TaskID - 1].TaskID;
+	}
+	return StatusMsg;
+	
 }
 
-StatusType GetTaskState(TaskType TaskID, TaskStateRefType State)
+StatusType GetTaskState(TaskType TaskID, TaskStateRefType State) 
 {
+	StatusType StatusMsg = E_OK;
 
+	if (TaskID > MAX_TASKS || TaskID < 1)
+	{
+		StatusMsg = E_OS_STATE;
+	}
+	else
+	{
+		*State = OsTasksPCB[TaskID - 1].State; // Note: implement PCB later
+	}
+	return StatusMsg;
 }
 
 void DeclareTask(int x)
