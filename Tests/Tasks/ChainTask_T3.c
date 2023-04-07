45d4f85d68e325a323bd9a4610c47b8ed5411017
Mahmoud-Matar <70941488+Mahmoud-Matar@users.noreply.github.com>
2023-04-07 18:04:48 +0200

    Add files via upload

---
+++ Tests/Tasks/ChainTask_T3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "Tasks.h"
+
+int main() {
+    struct Task T1 =
+    { 0,RUNNING,5,5,TASK_NON,1,1 };
+    struct Task T2 =
+    { 1,SUSPENDED,4,4,TASK_NON,1,1 };
+
+    RunningTaskID = 0;
+
+    OsTasksPCB[0] = &T1;
+    OsTasksPCB[1] = &T2;
+
+    
+
+
+    StatusType st = ChainTask(256);
+
+    printf("error_msg %d \n", st);
+    printf("RunningTaskID %d \n", RunningTaskID);
+    printf("state %d \n", T2.State);
+    printf("pointer state %d \n", OsTasksPCB[1]->State);
+    printf("pointer record %d \n", OsTasksPCB[1]->Activation_Record);
+
+    if(st != E_OS_LIMIT)
+    {
+		printf("Error Occured\n\n");
+		exit(EXIT_FAILURE);
+    }
+
+
+	return 0;
+}
