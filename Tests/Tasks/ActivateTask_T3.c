f326d4162b36b81a73e59be731c4644af729c839
dragonfireym <111197551+dragonfireym@users.noreply.github.com>
2023-04-14 23:40:41 +0200

    Update ActivateTask_T3.c

--- Tests/Tasks/ActivateTask_T3.c
+++ Tests/Tasks/ActivateTask_T3.c
@@ -5,14 +5,14 @@
 
 int main(){
     struct Task T1 =
-    {0,0, 0,SUSPENDED,5,5,TASK_NON,0,1,0 };
-    OsTasksPCB[0] = &T1;
+    { 0,0, 255,SUSPENDED,5,5,TASK_NON,0,1,0 };
+    OsTasksPCB[255] = &T1;
     StatusType st = ActivateTask(T1.ID);
     printf("error_msg %d \n", st);
     printf("state %d \n", T1.State);
-    printf("pointer state %d \n", OsTasksPCB[0]->State);
-    printf("pointer record %d \n", OsTasksPCB[0]->Activation_Record);
-    if(st != E_OS_ID)
+    printf("pointer state %d \n", OsTasksPCB[255]->State);
+    printf("pointer record %d \n", OsTasksPCB[255]->Activation_Record);
+    if (st != E_OS_ID)
     {
         printf("Error Occured\n\n");
         exit(1);
