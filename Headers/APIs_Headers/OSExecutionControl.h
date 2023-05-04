d4be2cc9e5788711f8bce3e1b477054a6ca374d1
Mahmoud-Matar <70941488+Mahmoud-Matar@users.noreply.github.com>
2023-05-04 08:24:03 -0800

    Update OSExecutionControl.h

--- Headers/APIs_Headers/OSExecutionControl.h
+++ Headers/APIs_Headers/OSExecutionControl.h
@@ -8,7 +8,7 @@
 
 AppModeType GetActiveApplicationMode(void);
 void StartOS (AppModeType Mode); // how to pass a deafult value argument to it? OSDEFAULTAPPMODE
-void ShutDownOS (StatusType Error)
+void ShutDownOS (StatusType Error);
 
 
 #endif
