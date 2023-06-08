4b3cb139ad5864c11c304d1fa22427f795adfa8b
Mahmoud-Matar <70941488+Mahmoud-Matar@users.noreply.github.com>
2023-06-08 11:03:34 -0800

    Update TaskConfig.h

--- Configurations/TaskConfig.h
+++ Configurations/TaskConfig.h
@@ -1,12 +1,24 @@
 #ifndef Configurations_TaskConfig_H
 #define Configurations_TaskConfig_H
 
+/**
+ * @brief The  maximum number of tasks in the system.
+ */
 #define MAX_TASKS 253
 
+/**
+ * @brief The highest priority a task can get.
+ */
 #define TASK_MAX_PRIO_NUM 5
 
+/**
+ * @brief The  lowest  priority a task can get.
+ */
 #define TASK_MIN_PRIO_NUM 0
 
+/**
+ * @brief The  number of tasks that share the same prio
+ */
 #define MAX_TASK_PER_PRIO 3
 
 #endif
