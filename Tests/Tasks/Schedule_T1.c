ca041a000513d89244b935bc2dbfdd863f40c327
Ziyad_Ahmed02 <ZiyadAhmed@aucegypt.edu>
2023-06-19 16:09:40 +0300

    Schedule_T1 Done

--- Tests/Tasks/Schedule_T1.c
+++ Tests/Tasks/Schedule_T1.c
@@ -1,17 +1,10 @@
 #include "../../Headers/APIs_Headers/Tasks.h"
 #include "../../Sources/APIs_Sources/Tasks.c"
 
-struct Task T1 = { 10,0,0,SUSPENDED,5,5,TASK_FULL,0,0,1,1 };
-struct Task T2 = { 20,0,1,SUSPENDED,1,1,TASK_NON,0,0,1,1 };
-struct Task T3 = { 30,0,2,SUSPENDED,2,2,TASK_FULL,0,0,1,1 };
-struct Task T4 = { 40,0,3,SUSPENDED,3,3,TASK_NON,0,0,1,1 };
-struct Task T5 = { 50,0,4,SUSPENDED,4,4,TASK_FULL,0,0,1,1 };
-struct Task T6 = { 60,0,5,SUSPENDED,5,5,TASK_NON,0,0,1,1 };
-struct Task T7 = { 70,0,6,SUSPENDED,1,1,TASK_FULL,0,0,1,1 };
-struct Task T8 = { 80,0,7,SUSPENDED,2,2,TASK_NON,0,0,1,1 };
-struct Task T9 = { 90,0,8,SUSPENDED,3,3,TASK_FULL,0,0,1,1 };
-struct Task T10 = { 100,0,9,SUSPENDED,4,4,TASK_NON,0,0,1,1 };
-
+struct Task IDLE = {0, 0, IDLE_TASK, SUSPENDED, 0, 0, TASK_FULL, 0, 0, 200, 0};
+struct Task T1 = {10, 0, 0, SUSPENDED, 5, 5, TASK_FULL, 0, 0, 1, 1};
+struct Task T2 = {20, 0, 1, SUSPENDED, 1, 1, TASK_NON, 0, 0, 1, 1};
+struct Task T3 = {30, 0, 2, SUSPENDED, 2, 2, TASK_FULL, 0, 0, 1, 1};
 
 void fill_pcbtable();
 
@@ -19,79 +12,98 @@ void print_ready_queue();
 
 void print_PCB_Table();
 
-
 int main()
 {
-    //Testing Scheduler with tasks that are non preemptive and preemptive with different priorities 
-    
-    
+
+    // Testing Scheduler with tasks that are non preemptive and preemptive with different priorities
+
     fill_pcbtable();
+    StatusType st = ActivateTask(IDLE_TASK);
+    print_ready_queue();
+    if (st != E_OK)
+    {
+        printf("Error Occured in START OS EX1 \n\n");
+        exit(1);
+    }
+    if (RunningTaskID != IDLE_TASK)
+    {
+        printf("Error Occured in START OS EX2 \n\n");
+        exit(1);
+    }
 
     print_PCB_Table();
     print_ready_queue();
 
-    ActivateTask(2);
+    st = ActivateTask(2);
     print_ready_queue();
-
-    Schedule();
-
-    print_PCB_Table();
+    if (st != E_OK)
+    {
+        printf("Error Occured in T1 E_OK \n\n");
+        exit(1);
+    }
+    if (T3.State != RUNNING && IDLE.State != READY)
+    {
+        printf("Error Occured in T1 Act \n\n");
+        exit(1);
+    }
 
     ActivateTask(1);
-
-    Schedule();
-
-    print_PCB_Table();
+    print_ready_queue();
+    if (st != E_OK)
+    {
+        printf("Error Occured in T1 E_OK \n\n");
+        exit(1);
+    }
+    if (T3.State != RUNNING && T2.State != READY && IDLE.State != READY)
+    {
+        printf("Error Occured in T1 Act \n\n");
+        exit(1);
+    }
 
     ActivateTask(0);
-
-    Schedule();
-
-    print_PCB_Table();
-
     print_ready_queue();
+    if (st != E_OK)
+    {
+        printf("Error Occured in T1 E_OK \n\n");
+        exit(1);
+    }
+    if (T3.State != READY && T1.State != RUNNING && T2.State != READY && IDLE.State != READY)
+    {
+        printf("Error Occured in T1 Act \n\n");
+        exit(1);
+    }
+
+    printf("\n\n ALL tests passed \n\n");
     return 0;
 }
 
-
-
-
 void fill_pcbtable()
 {
-
-
-
+    OsTasksPCB[IDLE_TASK] = &IDLE;
     OsTasksPCB[0] = &T1;
     OsTasksPCB[1] = &T2;
     OsTasksPCB[2] = &T3;
-    OsTasksPCB[3] = &T4;
-    OsTasksPCB[4] = &T5;
-    OsTasksPCB[5] = &T6;
-    OsTasksPCB[6] = &T7;
-    OsTasksPCB[7] = &T8;
-    OsTasksPCB[8] = &T9;
-    OsTasksPCB[9] = &T10;
-
 }
 
 void print_ready_queue()
 {
     printf("Ready Queue: ");
-    struct Ready_Entry* current = Ready_Queue.Head;
-    while(current!=NULL)
+    struct Ready_Entry *current = Ready_Queue.Head;
+    while (current != NULL)
     {
-        printf("%d ",current->task->ID);
+        printf("%d ", current->task->ID);
 
-        current=current->Next;
+        current = current->Next;
     }
     printf("\n");
 }
 void print_PCB_Table()
 {
     printf("PCB Table: \n");
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 3; i++)
     {
         printf("id: %d, state: %d \n", OsTasksPCB[i]->ID, OsTasksPCB[i]->State);
     }
+    printf("id: %d, state: %d \n", OsTasksPCB[IDLE_TASK]->ID, OsTasksPCB[IDLE_TASK]->State);
     printf("\n\n\n");
 }
