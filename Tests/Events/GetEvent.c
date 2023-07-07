7299debdc7b9e0bc8c625d200a272f1b7faa32f3
alisobhy22 <64151548+alisobhy22@users.noreply.github.com>
2023-07-07 20:50:29 +0300

    test

--- Tests/Events/GetEvent.c
+++ Tests/Events/GetEvent.c
@@ -21,8 +21,8 @@ int main()
     OsTasksPCB[3] = &T3;
 
     printf("Before GetEvent\n\n");
-    // StatusType st = GetEvent(T0.ID,Event);
-    // printf("After GetEvent\n\n");
+    StatusType st = GetEvent(T0.ID,Event);
+    printf("After GetEvent\n\n");
     // if(st != E_OK)
     // {
     //     printf("Error Occured in E_OK EX1\n\n");
