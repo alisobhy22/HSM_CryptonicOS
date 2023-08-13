6bbcb1cc454803ba56b0f99e97cefc1ab0a0dd80
Ziyad-Ahmed02 <70940282+Ziyad-Ahmed02@users.noreply.github.com>
2023-08-13 19:12:58 +0300

    Update WaitEvent.c

--- Tests/Events/WaitEvent.c
+++ Tests/Events/WaitEvent.c
@@ -5,10 +5,11 @@
 
 int main()
 {
-     EventMaskType EventMask[MAX_EVENTS] = {0, 1, 0, 1, 0};
-    struct Task T0 = {0, 0, 0, WAITING, 3, 3, TASK_FULL, 0, 0, 1, 3, 1, NULL}; // normal extended task with reasources
-    struct Task T1 = {0, 0, 1, WAITING, 4, 4, TASK_FULL, 0, 0, 1, 0, 0, NULL}; // not extended task
-    struct Task T2 = {0, 0, 2, WAITING, 5, 5, TASK_FULL, 0, 0, 1, 0, 1, NULL}; // normal extended task
+     struct Event EventMask = {0b11111,0,0};
+     EventMaskType WaitMask=0b01010;
+    struct Task T0 = {0, 0, 0, WAITING, 3, 3, TASK_FULL, 0, 0, 1, 3, 1, EventMask}; // normal extended task with reasources
+    struct Task T1 = {0, 0, 1, WAITING, 4, 4, TASK_FULL, 0, 0, 1, 0, 0, EventMask}; // not extended task
+    struct Task T2 = {0, 0, 2, WAITING, 5, 5, TASK_FULL, 0, 0, 1, 0, 1, EventMask}; // normal extended task
     struct Task IDLE = {0, 0, IDLE_TASK, SUSPENDED, 0, 0, TASK_FULL, 0, 0, 200, 0};
     
     OsTasksPCB[IDLE_TASK] = &IDLE;
@@ -18,14 +19,14 @@ int main()
     OS_ActivateTask(IDLE.ID);
 
     OS_ActivateTask(T0.ID);
-    StatusType st =  WaitEvent(EventMask);
+    StatusType st =  WaitEvent(WaitMask);
 
     if(st!=E_OS_RESOURCE)
     {
         printf("Error Occured in E_OS_RESOURCE EX1\n\n");
         exit(1);
     }
-    if(T0.Waiting_Events!=NULL)
+    if(T0.EventMask.Event_Waiting!=0)
     {
          printf("Error Occured in setting mask EX1\n\n");
         exit(1);
@@ -41,14 +42,14 @@ int main()
 
 
     OS_ActivateTask(T1.ID);
-     st =  WaitEvent(EventMask);
+     st =  WaitEvent(WaitMask);
 
     if(st!=E_OS_ACCESS)
     {
         printf("Error Occured in E_OS_ACCESS EX2\n\n");
         exit(1);
     }
-    if(T1.Waiting_Events!=NULL)
+    if(T1.EventMask.Event_Waiting!=0)
     {
          printf("Error Occured in setting mask EX2\n\n");
         exit(1);
@@ -62,14 +63,14 @@ int main()
 
 
      OS_ActivateTask(T2.ID);
-     st =  WaitEvent(EventMask);
+     st =  WaitEvent(WaitMask);
 
     if(st!=E_OK)
     {
         printf("Error Occured in E_OK EX3\n\n");
         exit(1);
     }
-    if(T2.Waiting_Events!=EventMask)
+    if(T2.EventMask.Event_Waiting!=WaitMask)
     {
          printf("Error Occured in setting mask EX3\n\n");
         exit(1);
